3fd8789a43305bac557873da330f4bd871ab1ed1
oliverlee96 <oliverleej@hotmail.com>
2024-08-09 16:32:27 +0100

    implement sessions

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const path = require ('path');
 
 const express = require('express');
+const csrf = require('csurf');
+const expressSession = require('express-session');
 
+const createSessionConfig = require('./config/session');
 const db = require('./data/database');
+const addCsrfTokenMiddleware = require('./middleware/csrf-token');
+const errorHandlerMiddleware = require('./middleware/errors');
 const authRoutes = require('./routes/auth.routes')
 
 const app = express();
 
+//Sets up Express views to use
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));  //ensures the public folder is served statically
 app.use(express.urlencoded({ extended: false })); //handling submitted form data
 
+//Session management
+const sessionConfig = createSessionConfig();
+app.use(expressSession(sessionConfig));
+
+//CSRF token management
+app.use(csrf());
+app.use(addCsrfTokenMiddleware);
+
+//Authentication routes like sign up, login, etc
 app.use(authRoutes);
 
+app.use(errorHandlerMiddleware);
+
+//Mongo connection 
 db.connectToDatabase().then(function() {
     app.listen(3000); //only connects if successful connection
 })
