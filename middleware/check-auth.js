a5f4eb5d59a51404d1cd1324cd8ea41d4b2c23b1
oliverlee96 <oliverleej@hotmail.com>
2025-01-20 15:59:51 +0000

    admin authorization

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,12 +2,13 @@
 function checkAuthStatus(req, res, next) {
     const uid = req.session.uid;
 
-    if (!uid) {
+    if (!uid) { // check if there is existing user id
         return next();
     }
-
+    //checking the session for user id, if theyre authenticated and if they are an admin
     res.locals.uid = uid;
     res.locals.isAuth = true; // set to confirm the user is authenticated
+    res.locals.isAdmin = req.session.isAdmin;
     next();
 }
 
